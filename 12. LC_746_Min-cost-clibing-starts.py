40e785d75b1597a7e1785ccef2339f5f3f8ba755
MyeongSeokKo <kms86989408@gmail.com>
2026-03-28 15:23:48 +0900

    chore:edit-LC746

--- 12. LC_746_Min-cost-clibing-starts.py
+++ 12. LC_746_Min-cost-clibing-starts.py
@@ -9,7 +9,7 @@ class Solution:
         dp[0] = cost[0]
         dp[1] = cost[1]
 
-        for in range(2, n):
+        for i  in range(2, n):
             dp[i] = cost[i] + min(dp[i - 1], dp[i-2])
 
         return min(dp[n-1], dp[n-2])
